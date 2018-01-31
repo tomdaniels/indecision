7660f799295103905ab7d53d020e54eebba819d3
tomdaniels <thomsondaniels@hotmail.com>
2018-01-31 17:31:08 +1100

    creating sub-classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -5,7 +5,6 @@ class Person {
         this.age = age;
     }
     getGreeting() {
-        // return 'Hi I am '+ this.name + '!';
         return `Hi, I am ${this.name}!`;
     }
     getDescription() {
@@ -13,8 +12,27 @@ class Person {
     }
 };
 
-const me = new Person('Tom Daniels', 22);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return this.major = !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` and their major is ${this.major}`
+        }
+
+        return description;
+    }
+}
+
+const me = new Student('Tom Daniels', 22, 'IT');
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Student();
 console.log(other.getDescription());
