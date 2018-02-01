971242f15e38d7a78647e9be6cfc21df0a8742ad
tomdaniels <thomsondaniels@hotmail.com>
2018-02-01 20:50:04 +1100

    sets up handle random pick method

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 class IndecisionApp extends React.Component {
     constructor(props) {
         super(props);
+        this.handlePick = this.handlePick.bind(this);
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
         this.state = {
             options: [
@@ -10,6 +11,7 @@ class IndecisionApp extends React.Component {
             ]
         }
     }
+
     handleDeleteOptions() {
         this.setState(() => {
             return {
@@ -17,6 +19,13 @@ class IndecisionApp extends React.Component {
             };
         });
     }
+
+    handlePick() {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        alert(option);
+    }
+
     render() {
         const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer';
@@ -24,7 +33,10 @@ class IndecisionApp extends React.Component {
         return (
           <div>
               <Header title={title} subtitle={subtitle} />
-              <Action hasOptions={this.state.options.length > 0} />
+              <Action
+                  hasOptions={this.state.options.length > 0}
+                  handlePick={this.handlePick}
+              />
               <Options
                   options={this.state.options}
                   handleDeleteOptions={this.handleDeleteOptions}
@@ -47,15 +59,12 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-    handlePick() {
-        alert('handlePick');
-    }
     render() {
         return (
           <div>
               <button
                   disabled={!this.props.hasOptions}
-                  onClick={this.handlePick}
+                  onClick={this.props.handlePick}
               >
                   What should I do?
               </button>
