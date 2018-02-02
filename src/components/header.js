c6bea05301ef9a7dd1763dfb2041b2fbd26b0a52
tomdaniels <thomsondaniels@hotmail.com>
2018-02-02 23:27:55 +1100

    styles architecture

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div>
-        <h1>{props.title}</h1>
+    <div className="header__wrap">
+        <h1 className="header__title">{props.title}</h1>
         {
             props.subtitle &&
-            <h2>{props.subtitle}</h2>
+            <h2 className="header__subtitle">{props.subtitle}</h2>
         }
     </div>
 );
