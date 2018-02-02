f9007fa1cedac1047e61f5438e10ae85d3535bbb
tomdaniels <thomsondaniels@hotmail.com>
2018-02-02 22:57:11 +1100

    refactor

--- src/components/options.js
+++ src/components/options.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import Option from './single-option.js';
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
-            {
-                props.options.length === 0 &&
-                <p>Please add an option to get started!</p>
-            }
-            {
-                props.options.map((option) => (
-                    <Option
-                        key={option}
-                        optionText={option}
-                        removeOption={props.removeOption}
-                    />
-                ))
-            }
-        </div>
-    );
-};
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {
+            props.options.length === 0 &&
+            <p>Please add an option to get started!</p>
+        }
+        {
+            props.options.map((option) => (
+                <Option
+                    key={option}
+                    optionText={option}
+                    removeOption={props.removeOption}
+                />
+            ))
+        }
+    </div>
+);
+
 
 export default Options;
--- src/components/single-option.js
+++ src/components/single-option.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-const Option = (props) => {
-    return(
-        <div>
-            {
-                props.optionText
-            }
-            <button
-                onClick={(e) => {
-                    props.removeOption(props.optionText)
-                }}
-            >
-                remove
-            </button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        {
+            props.optionText
+        }
+        <button
+            onClick={(e) => {
+                props.removeOption(props.optionText)
+            }}
+        >
+            remove
+        </button>
+    </div>
+);
 
 export default Option;
