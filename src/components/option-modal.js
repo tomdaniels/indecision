951655921723b5afc95b5fc81c8357fb3f7efe71
tomdaniels <thomsondaniels@hotmail.com>
2018-02-03 01:04:22 +1100

    modal complete

--- src/components/option-modal.js
+++ src/components/option-modal.js
@@ -8,13 +8,15 @@ const OptionModal = (props) => (
         onRequestClose={props.handleClearSelectedOption}
         contentLabel="Selected Option"
         closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
+        <h3 className="modal__title">Selected Option</h3>
         {
             props.selectedOption &&
-                <p>{props.selectedOption}</p>
+                <p className="modal__body">{props.selectedOption}</p>
         }
         <button
+            className="button"
             onClick={props.handleClearSelectedOption}
         >
             Okay
