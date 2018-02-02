2644891a385d6367f796f5bfd5bae674b6cc240c
tomdaniels <thomsondaniels@hotmail.com>
2018-02-02 20:56:35 +1100

    webpack-setup

--- src/indecision.js
+++ src/indecision.js
@@ -1,1 +1,14 @@
-console.log('indecision.js is running!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+          <div>
+              test
+          </div>
+        );
+    }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
